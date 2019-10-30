8ddb073d39848fede802dc44ab7c06b9ada8d85f
Christopher McVeigh <ctm684@cs.bham.ac.uk>
2019-10-30 21:20:19 +0000

    Initial commit + Week 5 Log

---
+++ README.md
@@ -0,0 +1,7 @@
+#Sentiment Analysis vs. Human Inference for Detecting Sarcasm
+##Chris McVeigh
+###University of Birmingham
+
+Undergraduate project for year 2019-20
+
+SARC corpus by Mikhail Khodak, Nikunj Saunshi, Kiran Vodrahalli available from https://nlp.cs.princeton.edu/SARC/2.0
---
+++ code/week5/test.py
@@ -0,0 +1,36 @@
+import ijson
+import csv
+
+folder = r"C:\Users\Chris\Documents\Final Year Project"
+
+if __name__ == '__main__':
+
+    i = 0;
+    rows = []
+    with open(folder+r"\SARC\main\train-balanced.csv",'r') as g:
+        reader = csv.reader(g, delimiter='|')
+        for row in reader:
+            row.append(row[0])
+      
+    with open(folder+r"\SARC\main\comments.json",'r') as f:
+        parser = ijson.parse(f)
+        for prefix, event, value in parser:
+            if "." in prefix:
+                sub = prefix.split(".")
+                if sub[1] == "subreddit":
+                    print(i, ". ", value,sep="")
+                    i += 1
+                    if i > 500:
+                        break
+
+        # Prints entire structure
+        ##parser = ijson.parse(f)
+        ##for prefix, event, value in parser:
+        ##print('prefix={}, event={}, value={}'.format(prefix, event, value))
+
+        # Gets specific item - loops through entire file on every run so not ideal.
+        ##for row in rows:
+        ##    item = ijson.items(row)
+        ##    for s in item:
+        ##        print(s["subreddit"])
+        
